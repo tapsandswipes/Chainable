64bcb5e720427fbcb8031922f2fe7772277c46f5
Antonio Cabezuelo Vivo <antonio@tapsandswipes.com>
2021-04-05 13:27:40 +0200

    Add some tests
    Signed-off-by: Antonio Cabezuelo Vivo <antonio@tapsandswipes.com>

--- Tests/ChainableTests/ChainableTests.swift
+++ Tests/ChainableTests/ChainableTests.swift
@@ -2,14 +2,74 @@ import XCTest
 @testable import Chainable
 
 final class ChainableTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Chainable().text, "Hello, World!")
+
+    struct ValueObj: Chainable {
+        var a: String
+        var b: Int
+    }
+    
+    class ClassObj: Chainable {
+        var a: String
+        var b: Int
+        
+        init(a: String, b: Int) {
+            self.a = a
+            self.b = b
+        }
     }
 
+    func testValueChain() {
+        let sut = ValueObj(a: "Hello", b: 0)
+        
+        let b = sut.then {
+            $0.a = "Goodbye"
+        }
+        
+        XCTAssertEqual(sut.a, "Hello")
+        XCTAssertEqual(sut.b, 0)
+        XCTAssertEqual(b.a, "Goodbye")
+        XCTAssertEqual(b.b, 0)
+    }
+
+    func testReferenceChain() {
+        let sut = ClassObj(a: "Hello", b: 0)
+
+        let b = sut.then {
+            $0.a = "Goodbye"
+        }
+
+        XCTAssert(sut === b)
+        XCTAssertEqual(sut.a, "Goodbye")
+        XCTAssertEqual(sut.b, 0)
+    }
+    
+    func testValueDo() {
+        let sut = ValueObj(a: "Hello", b: 0)
+
+        sut.do {
+            XCTAssertEqual($0.a, "Hello")
+            XCTAssertEqual($0.b, 0)
+        }
+        
+        XCTAssertEqual(sut.a, "Hello")
+        XCTAssertEqual(sut.b, 0)
+    }
+    
+    func testReferenceDo() {
+        let sut = ClassObj(a: "Hello", b: 0)
+        
+        sut.do {
+            XCTAssertEqual($0.a, "Hello")
+            XCTAssertEqual($0.b, 0)
+        }
+        
+        XCTAssertEqual(sut.a, "Hello")
+        XCTAssertEqual(sut.b, 0)
+    }
+
+    
     static var allTests = [
-        ("testExample", testExample),
+        ("testValueChain", testValueChain),
     ]
 }
+
